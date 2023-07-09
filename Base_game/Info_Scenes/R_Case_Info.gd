8f6b84217e39291c97814054ffe8af7a7333ff19
Florian Röder <florian@roeder2.de>
2023-07-09 17:37:24 +0200

    pic 2

--- Base_game/Info_Scenes/R_Case_Info.gd
+++ Base_game/Info_Scenes/R_Case_Info.gd
@@ -5,10 +5,12 @@ var sceneinstance = self
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var r_pp1_text_label = $RCaseinfoTexture/R_PP1_Text
+	var r_pp2_pic = $Waffendisplay
 	if GlobalVars.r_pp1_solved == false and GlobalVars.cur_r_plotpoint == "R_PP1":
-		pass
+		r_pp2_pic.visible = false
 	else:
 		r_pp1_text_label.visible = false
+		r_pp2_pic.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
