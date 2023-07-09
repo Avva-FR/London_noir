0895f4a59146b524bf5b6577c49bb3af9744a549
Florian Röder <florian@roeder2.de>
2023-07-09 16:07:19 +0200

    finalizing

--- Base_game/Info_Scenes/Left_Case_Info.gd
+++ Base_game/Info_Scenes/Left_Case_Info.gd
@@ -4,10 +4,17 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var l_pp1_text_label = $LCaseInfoText/L_PP1_Text
+	var lpp2_pic1 = $Koch
+	var lpp2_pic2 = $Messer
 	if GlobalVars.l_pp1_solved == false and GlobalVars.cur_l_plotpoint == "L_PP1":
-		pass
+		lpp2_pic1.visible = false
+		lpp2_pic2.visible = false
+		
 	else:
 		l_pp1_text_label.visible = false
+		lpp2_pic1.visible = true
+		lpp2_pic2.visible = true
+		
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # London_noir
 Learning Game "London Noir" Medida SS23
+Copyright 2023 FLorian Röder, Julius Göpfert
